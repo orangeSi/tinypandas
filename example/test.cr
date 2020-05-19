e3df594f03208911a5279d1c5446e3d11706a955
root <root@awesome-ponies-2.localdomain>
2020-05-19 04:44:19 -0400

    update

--- example/test.cr
+++ example/test.cr
@@ -5,7 +5,7 @@ raise "usage:./test yy.xls\n" if ARGV.size == 0
 ifile = ARGV[0]
 puts "intpu file #{ifile}"
 pd = Tinypandas.new
-df = pd.read_table(ifile, sep = "\t") # def read_table(filepath_or_buffer : String, sep = "\t", t : Int32|Bool = 0, delimiter : String = "\n", header : HeaderType = 0, index_col : IndexColType = 0, comment : String|Regex = "#", skiprows : SkiprowsType = false, skip_blank_lines : Bool = true)
+df = pd.read_table(ifile, sep: "\t") # def read_table(filepath_or_buffer : String, sep = "\t", t : Int32|Bool = 0, delimiter : String = "\n", header : HeaderType = 0, index_col : IndexColType = 0, comment : String|Regex = "#", skiprows : SkiprowsType = false, skip_blank_lines : Bool = true)
 puts "df is #{df}\n"
 puts "df.to_str is\n#{df.to_str}\n"
 puts "df[A2][B3] is #{df["A2"]["B3"]}\n"
